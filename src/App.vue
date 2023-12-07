812006ad60a551e2f49e4a42e57c61617071cf22
reuby-dev <delphineb57@gmail.com>
2023-12-07 17:41:16 +0100

    finish choices homepage

--- src/App.vue
+++ src/App.vue
@@ -20,19 +20,19 @@ export default {
 
 body {
   margin: 0;
+  background-color: #F4F6FA;
 }
 
 #app {
   @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&family=Rubik:ital,wght@0,400;0,500;1,300;1,400&display=swap');
   font-family: Rubik, Helvetica, Arial, sans-serif;
   font-weight: 300;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
 
 .max-container {
-  max-width: 1400px;
+  max-width: 1200px;
 }
 
 h1 {
--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -23,6 +23,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 50px;
 }
 
 </style>
